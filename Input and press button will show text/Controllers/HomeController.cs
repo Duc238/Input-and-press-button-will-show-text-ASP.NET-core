fd46ffc319fce9b601680d47937b3778d8dcd725
Windows <vtnduc2382000@gmail.com>
2023-12-03 10:26:06 +0700

    Add project files.

---
+++ Input and press button will show text/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Input_and_press_button_will_show_text.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Input_and_press_button_will_show_text.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ShowText(string text)
+        {
+            return Content(text);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Input and press button will show text/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Input_and_press_button_will_show_text.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
